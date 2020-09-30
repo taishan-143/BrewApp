0a564d4d778d840a19eeabc6f9c29c3c1272f1b8
Taishan Rowe <taishan_rowe314@outlook.com>
2020-09-30 15:28:50 +0100

    ive finally done it

--- src/task_load_db.py
+++ src/task_load_db.py
@@ -1,10 +1,11 @@
 import csv
+import pymysql
 
 # username = 1st, name = 2nd (split into first and last) 
 # job title = 4th, mobile = 8th, zip = 13th
 
 
-def load_required_data(filepath):     # load the csv file
+def data_extraction(filepath):     # load the csv file
     data_list = []
     try:
         with open(filepath, 'r') as data_file:
@@ -18,7 +19,8 @@ def load_required_data(filepath):     # load the csv file
         print("Failure opening file!")
     
 
-db_data = load_required_data('src/data/Import_User_Sample_en.csv')
+db_data = data_extraction('src/data/Import_User_Sample_en.csv')
+
     
 def data_transformation(data):
     final_data = []
@@ -27,7 +29,8 @@ def data_transformation(data):
         if data_list[1] == 'Name':
             data_list[1] = 'First Name'
             data_list.insert(2, 'Surname')
-        final_data.append(data_list)
+        # final_data.append(data_list)
+        continue
 
     for data_list in data[1:]:
         first_name = data_list[1].split()[0].title()
@@ -39,18 +42,49 @@ def data_transformation(data):
 
     return final_data
     
+
 data_transform = data_transformation(db_data)
+print(data_transform)
 
-def save_items_to_csv(filepath, data):
-    try:
-        with open(filepath, 'w', newline ='') as data_file:
-            csv_writer = csv.writer(data_file, quoting = csv.QUOTE_NONE)
-            for item in data:
-                csv_writer.writerow([item[0], item[1], item[2], item[3], item[4], item[5]])
-    except:
-        print("Failure opening file!")
+def data_loading_to_the_database(data):
+    connection = pymysql.connect(
+        host = 'localhost',
+        port = 33066, 
+        user = 'root', 
+        passwd = 'Qazwsx11',
+        database = "Brew_App"
+        )
+    cursor = connection.cursor()
+    for person_data in data:
+        #print(tuple(person_data))
+        cursor.execute("INSERT INTO PersonImport (User_Name, First_Name, Surname, Job_Title, Mobile_No, Post_Code) VALUES (%s, %s, %s, %s, %s, %s)", person_data)
 
-save_items_to_csv('src/data/Upload_to_database.csv', data_transform)
+    connection.commit()
+    # rows = cursor.fetchall()
+    cursor.close()
+    connection.close()
+
+data_loading_to_the_database(data_transform)
+
+
+
+
+
+
+
+
+
+
+# def save_items_to_csv(filepath, data):
+#     try:
+#         with open(filepath, 'w', newline ='') as data_file:
+#             csv_writer = csv.writer(data_file, quoting = csv.QUOTE_NONE)
+#             for item in data:
+#                 csv_writer.writerow([item[0], item[1], item[2], item[3], item[4], item[5]])
+#     except:
+#         print("Failure opening file!")
+
+# save_items_to_csv('src/data/Upload_to_database.csv', data_transform)
 
 
 
