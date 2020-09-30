2cf1e2fb1b9f60382c3df6e3c94666b669cd442b
Taishan Rowe <taishan_rowe314@outlook.com>
2020-09-30 10:09:30 +0100

    new work

--- src/Database_data.py
+++ src/Database_data.py
@@ -1,5 +1,10 @@
 import pymysql
 
+
+class Person:
+    def __init__(self):
+        pass
+
 def Person_DB():
     connection = pymysql.connect(
         host = 'localhost',
@@ -9,14 +14,18 @@ def Person_DB():
         database = "Brew_App"
         )
     cursor = connection.cursor()
-    cursor.execute("SELECT Person_Name, Person_Age FROM Person")
+    cursor.execute("SELECT PersonID, Person_Name FROM Person")
     connection.commit()
     rows = cursor.fetchall()
     cursor.close()
     connection.close()
 
     for row in rows:
-        print(row)
+        people_dic = {}
+        ID, Name = PersonID, Person_Name
+        people_dic[ID] = Name
+
+
 
 def Drink_DB():
     connection = pymysql.connect(
@@ -34,9 +43,11 @@ def Drink_DB():
     connection.close()
 
     for row in rows:
-        print(row)
+        people_dic_list = []
+        people_dic_list.append(row)
+        people_dic = dict(people_dic_list)
 
 
 Person_DB()
 print("\n")
-Drink_DB()
+#Drink_DB()
